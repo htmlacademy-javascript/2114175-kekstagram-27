a05bf795e18f9e8b7ff6d886ba5731b2810cc671
Keks <keks@htmlacademy.ru>
2022-11-04 18:48:51 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,28 @@
+import {renderListing} from './listing.js';
+import {showAlert} from './util.js';
+import {showModal} from './details.js';
+import {registerUploadFormEvents} from './upload-form.js';
+
+const onPhotoLoaded = (photos) => {
+  renderListing(photos);
+  const thumbnails = document.querySelectorAll('.picture');
+  const addThumbnailClickHandler = function (thumbnail, photo) {
+    thumbnail.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      showModal(photo);
+    });
+  };
+  for (let i = 0; i <= thumbnails.length - 1; i++) {
+    addThumbnailClickHandler(thumbnails[i], photos[i]);
+  }
+
+  // Регистрируем события для формы загрузки
+  registerUploadFormEvents();
+};
+
+// Загрузка фотографий
+fetch('https://27.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then(onPhotoLoaded)
+  .catch(() => showAlert('Не удалось загрузить фотографии'));
+
